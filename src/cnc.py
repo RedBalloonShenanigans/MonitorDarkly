14ae2181f768a22e2171743a47e7b6fb113945ec
Connor Abbott <cwabbott0@gmail.com>
2016-08-08 20:32:16 -0400

    cnc: use put_image for displaying multiple images
    Rather than hand-rolling it ourselves.

--- src/cnc.py
+++ src/cnc.py
@@ -30,41 +30,16 @@ dell.initialize()
 dell.debug_on()
 
 if args.paypal_demo:
-    image = DellImage("lock_https.gif")
-    image_colors = image.colors
-    metainfo, _ = delltools.upload_single_image(dell, image, 0x600000)
-    clut_table = metainfo[4]
-    width = metainfo[0]
-    height = metainfo[1]
-    stride = metainfo[2]
-    upload_address = metainfo[3]
-    # delltools.clear_0xc000(dell)
-    delltools.sdram_write(dell, src=0x600000,
-                          dest=0, height=height, width=width,
-                          dest_stride=stride)
-    delltools.transfer_clut(dell, clut_table)
-    control = get_control_struct(width, height, 0x63, 0x4a, 0)
-    control = control[:0x26] + '\x14' + control[0x27:]
-    delltools.mem_write(dell, 0xc078, control)
+    lock_image = DellImage("lock_https.gif")
+    lock_metainfo, _ = delltools.upload_single_image(dell, lock_image, 0x600000)
+    delltools.put_image(dell, lock_metainfo, 0x63, 0x4a)
     time.sleep(2)
     # put default image
-    image = DellImage("amount_image.png", 255 - image_colors)
-    image.image = "".join(chr(ord(b) + image_colors) for b in image.image)
-    metainfo, _ = delltools.upload_single_image(dell, image, 0x600000)
-    clut_table = metainfo[4]
-    width = metainfo[0]
-    height = metainfo[1]
-    stride = metainfo[2]
-    upload_address = metainfo[3]
-    # delltools.clear_0xc000(dell)
-    delltools.sdram_write(dell, src=0x600000,
-                          dest=0x1000, height=height, width=width,
-                          dest_stride=stride)
-    delltools.transfer_clut(dell, clut_table, clut_offset=image_colors * 4)
-    control = get_control_struct(width, height, 134, 330, 0x1000>>6)
-    control = control[:0x26] + '\x14' + control[0x27:]
-    delltools.mem_write(dell, 0xc1e0, control)
-
+    amount_image = DellImage("amount_image.png", 255 - lock_image.colors)
+    amount_metainfo, _ = delltools.upload_single_image(dell, amount_image, 0x600000,
+                                                       clut_offset=lock_image.colors)
+    delltools.put_image(dell, amount_metainfo, 134, 330, sdram_loc=0x1000 >> 6,
+                        tile=1)
 
 
 delltools.execute_payload(dell, payload, 0x6000)
